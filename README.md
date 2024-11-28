3857aa6d0eccccac57da52ff01e5d28b06c5b7e8
Shree Sudame <sudameshree8@gmail.com>
2024-11-28 17:34:49 +0530

    RAG-Multi-Query Demo Done

---
+++ README.md
@@ -0,0 +1,43 @@
+## RAG Pipeline for confidential Edtech documents
+### LangChain, LangSmith, Chroma and FAISS Vector store, Ollama (llama3.2), Streamlit.
+
+
+
+* Notion of a Prompt
+>> Placeholder where we pass custom instructions/behavior for the LLM based on the context and question asked by the user.
+Example of a normal prompt :
+'''
+template = 
+Answer the question based on the provided content only 
+Think Step by step before providing a detailed response: 
+{context}
+
+Question : {question}
+'''
+
+
+* Getting started with Query Translation
+It involves approaches like Multi-Query and RAG Fusion
+
+a) Multi-Query involves breaking down the question into different sub-questions and the taking the union of the context obtained for those questions and finally passing it to the original question.
+Example Prompt :
+'''
+You are an AI language model assistant. Your task is to generate five different versions of the given user question 
+to retrieve relevant documents from a vector store/database. By generating multiple perspectives on the user questions, your goal
+is to help the user overcome some of the limitations of the distance-based similarity search.
+Provide these alternative questions seperated by newlines. Original Question : {question}
+'''
+
+Later Steps:
+a) Create a chain that includes the following:
+ prompt_perspectives  
+    | llm
+    | StrOutputParser()
+    | (lambda x: x.split("\n")) [Creates a list of questions]
+
+>> All this to get those distinct questions
+
+b) Later we create a function that would combine the responses to those questions and would be feed as an entire corpus to the original question with the retriever
+ generate_questions | retriever.map() | get_unique_docs
+
+>> This corpus would serve as context from which the original question would be answered. 
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 
-from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
+
 from langchain_core.output_parsers import StrOutputParser
 from langchain_ollama.llms import OllamaLLM
 
@@ -20,15 +20,19 @@ langchain_project = os.getenv('LANGCHAIN_PROJECT')
 DATA_PATH = 'data-pdf\optimizers.pdf'
 CHROMA_PATH = 'chroma'
 
-# C:\Users\Shree123\rag-demo\data-pdf\
 
+
+# Loading data from the pdfs'
 from load_and_split import load_content, split_content
 raw_chunks = load_content(DATA_PATH)
-
 chunks = split_content(raw_chunks)
 
+
+
+# Initializing Vector Store
 from vector_store import chroma_vector_store
 chroma_db = chroma_vector_store(CHROMA_PATH)
+
 
 
 # Generating unique ids for individual chunks
@@ -37,32 +41,74 @@ uuids = [str(uuid4()) for _ in range(len(chunks))]
 print(f'Number of Chunks : {len(chunks)}')
 
 
-# Retreival
 
-# retriever = chroma_db.as_retriever(search_type="mmr")
+# # Retreival
+# # retriever = chroma_db.as_retriever(search_type="mmr")
 retriever = chroma_db.as_retriever(search_kwargs={"k": 1})
 
-input_query = "Describe in detail the process of SGD"
 
-# docs = retriever.invoke(input_query)
-# print(len(docs))
 
-# for res in docs:
-#     print(f'Content -> {res.page_content} , source -> {res.metadata}')
-
-# # # # # # # ## # # ## # # ## # # ## # # ## # # ## # # ## # # ## # # ## # # ## # # ## # # ## # # ## # # ## # # #
-
-# Generation
 from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.output_parsers import StrOutputParser
+# Passing Prompt for Query Translation -> Multi-Query for perspectives
+perspective_template = """
+You are an AI language model assistant. Your task is to generate five different versions of the given user question 
+to retrieve relevant documents from a vector store/database. By generating multiple perspectives on the user questions, your goal
+is to help the user overcome some of the limitations of the distance-based similarity search.
+Provide these alternative questions seperated by newlines. Original Question : {question}
+"""
+prompt_perspectives = ChatPromptTemplate.from_template(perspective_template)
+
+
+# Generation of Perspective Questions
+model_name = "llama3.2"
+ollama_url = "http://localhost:11434/"
+
+llm = OllamaLLM(model = model_name, 
+                    base_url = ollama_url,
+                    temperature = 0.2,
+                )
+
+# Chain for Perspective Questions
+generate_questions = (
+    prompt_perspectives
+    | llm
+    | StrOutputParser()
+    | (lambda x: x.split("\n"))
+)
+
+
+
+# To get the union of the unique documents (context not questions) obtained.
+from langchain.load import dumps, loads
+
+def get_unique_docs(documents : list[list]):
+    """ Union of all the documents """
+    flattened_docs = [dumps(docs) for sublist in documents for docs in sublist]
+    unique_docs = list(set(flattened_docs))
+    load_unique_docs = [loads(ud) for ud in unique_docs]
+    return load_unique_docs
+
+
+
+# Retrieve Questions
+question = "What are the different optimizers mentioned in the Documents ? Please explain each one of them"
+retrieval_chain = generate_questions | retriever.map() | get_unique_docs
+# docs = retrieval_chain.invoke({'question' : question})
+# print(docs) , print(len(docs))
+
+
+
+from operator import itemgetter
+from langchain_core.runnables import RunnablePassthrough
 
 # prompt
 template = """
-Answer the question based on the provided content only 
-Think Step by step before providing a detailed response: 
+Answer the following question based on this context:
+
 {context}
 
-Question : {question}
+Question: {question}
 """
 
 prompt = ChatPromptTemplate.from_template(template)
@@ -76,15 +122,16 @@ llm = OllamaLLM(model = model_name,
                     temperature = 0.2,
                 )
 
-from langchain_core.runnables import RunnablePassthrough
+
 # Chain
-retrieval_chain = (
-    {"context" : retriever, "question" : RunnablePassthrough()}
+final_rag_chain = (
+    {"context" : retrieval_chain,
+    "question" : itemgetter('question')}
     | prompt 
     | llm 
     | StrOutputParser()
 )
 
-response = retrieval_chain.invoke(input_query)
+response = final_rag_chain.invoke({'question': question})
 
 print(response)
